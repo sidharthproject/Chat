b619b1d56fc83b67f9a325d011a0914097668ada
sidharthproject <sidharthpatro001@gmail.com>
2024-01-15 19:29:09 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Register from './pages/Register'
 import { useSelector } from 'react-redux'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login' 
 import { createBrowserRouter } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
