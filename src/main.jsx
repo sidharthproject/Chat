0f0d6ee7013ae4fc2207f56710bfd5685bad23eb
sidharthproject <sidharthpatro001@gmail.com>
2024-01-15 19:11:26 +0530

    third copmmit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter,Navigate,RouterProvider} from 'react-router-dom'
-import Home from './pages/home'
- import Login from './pages/Login'
- import Register from './pages/Register'
- import { Provider } from 'react-redux'
- import store from './store/store.js'
- import { useSelector } from 'react-redux'
+
+
 
 
 
